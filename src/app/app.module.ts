e2ea71bab1c42b77c513e9acb42bfb2b467453a2
Fernando Rodríguez <ferez@iMac-de-Fernando.local>
2020-05-09 02:24:47 +0200

    feat(app): init mwc top bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./component/app.component";
 import { NavigationComponent } from "./navigation/component/navigation.component";
 import { ScullyLibModule } from '@scullyio/ng-lib';
 
+import '@material/mwc-top-app-bar-fixed';
+
 @NgModule({
   declarations: [AppComponent, NavigationComponent],
   imports: [BrowserModule, AppRoutingModule, ScullyLibModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
 export class AppModule {}

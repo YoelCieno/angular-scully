8cfe5d2d73ca1eb6a9b70f7f22daf373f12f6abb
Fernando Rodríguez <ferez@iMac-de-Fernando.local>
2020-06-07 01:40:04 +0200

    feat(app): minor css styles accent change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", loadChildren: () => import("./home/home.module").then(m => m.HomeModule) },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
-  { path: "about", loadChildren: () => import("./about/about.module").then(m => m.AboutModule) },
-  { path: "contact", loadChildren: () => import("./contact/contact.module").then(m => m.ContactModule) }
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {}

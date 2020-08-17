69a70fd95daa552e6c55b0c1bb314b3325ae585a
Fernando Rguez <frodriguez@stratio.com>
2020-08-17 03:49:19 +0200

    test(styles): html font size changes

--- src/app/values/values.module.ts
+++ src/app/values/values.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ValuesComponent } from './component/values.component';
 import { ValuesRoutingModule } from './values-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ValuesComponent],
   imports: [
     CommonModule,
-    ValuesRoutingModule
+    ValuesRoutingModule,
+    MatButtonModule
   ],
-  exports: [ValuesComponent]
+  exports: [ValuesComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
 export class ValuesModule { }

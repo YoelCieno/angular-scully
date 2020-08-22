da4619b0f299bbccf21763232a270ddd12ca7ece
Fernando Rguez <frodriguez@stratio.com>
2020-08-22 14:29:31 +0200

    feat(values): title and layouts

--- src/app/values/component/values.component.ts
+++ src/app/values/component/values.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ValuesConstants } from '../values.constants';
 
 @Component({
   selector: 'app-values',
   templateUrl: './values.component.html',
   styleUrls: ['./values.component.scss']
 })
+
 export class ValuesComponent implements OnInit {
+
+  public texts: { title: string };
 
   constructor() { }
 
   ngOnInit(): void {
+    this.texts = { title: ValuesConstants.TEXTS.TITLE };
   }
-
 }
---
+++ src/app/values/values.constants.ts
@@ -0,0 +1,5 @@
+export class ValuesConstants {
+  static readonly TEXTS = {
+    TITLE: 'Nuestro legado en educación y cultura'
+  }
+}
